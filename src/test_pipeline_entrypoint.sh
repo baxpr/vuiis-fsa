88222c8ab787f9f64d937d67b65634d8adc302c2
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2026-09-09 16:36:01 -0500

    entry

--- src/test_pipeline_entrypoint.sh
+++ src/test_pipeline_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 MR=~/MATLAB/R2023a
-export LD_LIRBARY_PATH=$MR/runtime/glnxa64:$MR/bin/glnxa64:$MR/sys/os/glnxa64:$MR/sys/opengl/lib/glnxa64
+export LD_LIBRARY_PATH=$MR/runtime/glnxa64:$MR/bin/glnxa64:$MR/sys/os/glnxa64:$MR/sys/opengl/lib/glnxa64
 export PATH=$(pwd):$(pwd)/../matlab/bin:$PATH
 
 pipeline_entrypoint.sh
