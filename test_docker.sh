87f89731acaf93f2d8a9ba74dff9a7b68c3cca36
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2026-09-10 09:00:04 -0500

    tester

--- test_docker.sh
+++ test_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run \
+podman run \
     --mount type=bind,src=$(pwd -P)/INPUTS,dst=/INPUTS \
     --mount type=bind,src=$(pwd -P)/OUTPUTS,dst=/OUTPUTS \
     vuiis-fsa:test \
